f29addf8f54f5729f074b2b34ba91bf88839b6a4
robertw <r.wdowczyk@gmail.com>
2017-08-08 18:30:48 +0200

    dodanie plikow

---
+++ scripts.js
@@ -0,0 +1,17 @@
+﻿// scripts.js
+var a = 3;
+var b = 3;
+var exValue = (a * a) + (2 * a * b) - (b * b);
+console.log('ex field with base a: ' + a + ' and b: ' + b + ' is equal to: ' + exValue);
+var wynik = prompt(exValue);
+alert('Wynik to: ' + exValue);
+var wynik = 1;
+if ( wynik >= 1 ) {
+    console.log('Wynik dodatni');
+} else if ( wynik < 0 ) {
+    console.log('Wynik ujemny');
+} else if (wynik == 0) { 
+    console.log('Wynik to 0');
+} else { 
+console.log('Your result');
+}
